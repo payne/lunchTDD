528d3770285471ef9915aeb9a3f2b1eb936e6f77
Matt Payne <Payne@MattPayne.org>
2016-05-07 12:37:51 -0500

    create_group_spec.rb passes woot!

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,16 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
   end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_url, notice: 'Group has been created.'
+    end
+  end
+
+private
+  def group_params
+    params.require(:group).permit(:title,:description,:growing)
+  end
 end
