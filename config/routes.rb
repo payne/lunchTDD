8c3bfc0f59d5c8f13370057f76981505bb7debd6
Matt Payne <Payne@MattPayne.org>
2016-05-09 21:02:48 -0500

    groups_path now works in application.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :groups, only: [ :new, :create, :show ]
+  resources :groups, only: [ :new, :create, :show, :index ]
   root to: 'welcome#index'
 end
