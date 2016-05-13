36fea2d07781991fc32778918a7bf50be0b8d670
Matt Payne <Payne@MattPayne.org>
2016-05-13 06:02:30 -0500

    "rails g migration AddNameToLunchEvents name" and scaffold tweaking

--- app/controllers/lunch_events_controller.rb
+++ app/controllers/lunch_events_controller.rb
@@ -69,6 +69,6 @@ class LunchEventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def lunch_event_params
-      params.require(:lunch_event).permit(:lunchtime, :group_id)
+      params.require(:lunch_event).permit(:name, :lunchtime, :group_id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160513104822) do
+ActiveRecord::Schema.define(version: 20160513105643) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160513104822) do
     t.integer  "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   add_index "lunch_events", ["group_id"], name: "index_lunch_events_on_group_id"
