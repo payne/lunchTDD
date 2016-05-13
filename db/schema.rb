4cfb7f75404fe90dd807a1e3e6da550d19d0b4be
Matt Payne <Payne@MattPayne.org>
2016-05-13 05:56:00 -0500

    user has_many lunch_events through lunch_assignments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512025624) do
+ActiveRecord::Schema.define(version: 20160513104822) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,25 @@ ActiveRecord::Schema.define(version: 20160512025624) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "lunch_assignments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "lunch_event_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  add_index "lunch_assignments", ["lunch_event_id"], name: "index_lunch_assignments_on_lunch_event_id"
+  add_index "lunch_assignments", ["user_id"], name: "index_lunch_assignments_on_user_id"
+
+  create_table "lunch_events", force: :cascade do |t|
+    t.date     "lunchtime"
+    t.integer  "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "lunch_events", ["group_id"], name: "index_lunch_events_on_group_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
